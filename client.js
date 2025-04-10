d06bbc49fed0560f986aa82e28e3785b111b65c3
arthursereno <arthurbarbosa2809@gmail.com>
2025-04-10 16:26:26 -0300

    Create client.js

---
+++ client.js
@@ -0,0 +1,18 @@
+window.TrelloPowerUp.initialize({
+  'card-buttons': function(t, options) {
+    return [{
+      text: '4. 🐁 Cloaker',
+      callback: function(t) {
+        return t.card('id')
+          .then(function(card) {
+            const webhookUrl = `https://n8n.api-blogfeelgood.fun/webhook-test/criar-campanha-redtrack?cardId=${card.id}`;
+            return fetch(webhookUrl, {
+              method: 'GET'
+            })
+            .then(() => t.alert({ message: '✅ Dados do CLoaker Gerados!' }))
+            .catch(() => t.alert({ message: '⏳ Gerando...' }));
+          });
+      }
+    }];
+  }
+});
